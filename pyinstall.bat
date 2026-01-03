6d77489cd5d58279fffb87b591b116bfff645288
xilinq <230249476@seu.edu.cn>
2026-01-03 11:00:27 +0800

    v1.2

--- pyinstall.bat
+++ pyinstall.bat
@@ -2,6 +2,6 @@ RMDIR /S /Q build
 RMDIR /S /Q dist
 DEL /F /Q *.spec
 
-pyinstaller --name "PerformanceApp_v1.1" --onefile --windowed --collect-data matplotlib --paths . main.py
+pyinstaller --name "PerformanceApp_v1.2" --onefile --windowed --collect-data matplotlib --paths . main.py
 
 pause
